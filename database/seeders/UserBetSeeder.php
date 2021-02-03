2d1b6a54ac301761896ce4ce95755fc9ef543b5a
CablePoolV4 <vegitoson01@gmail.com>
2021-02-04 00:25:12 +0700

    seeder for user bet

--- database/seeders/UserBetSeeder.php
+++ database/seeders/UserBetSeeder.php
@@ -14,16 +14,28 @@ class UserBetSeeder extends Seeder
      */
     public function run()
     {
-        for($i = 1; $i <= 20; $i++)
-        {
-            $user_id = rand(1, 3);
-            $coin = rand(10, 500);
-            $dice_value = rand(0, 5);
-            UserBet::create([
-                'user_id' => $user_id,
-                'coin' => $coin,
-                'dice_value' => $dice_value
-            ]);
-        }
+        UserBet::create([
+            'user_id' => "1",
+            'coin' => "10",
+            'dice_value' => "0"
+        ]);
+
+        UserBet::create([
+            'user_id' => "2",
+            'coin' => "100",
+            'dice_value' => "1"
+        ]);
+
+        UserBet::create([
+            'user_id' => "3",
+            'coin' => "1000",
+            'dice_value' => "3"
+        ]);
+
+        UserBet::create([
+            'user_id' => "3",
+            'coin' => "5000",
+            'dice_value' => "4"
+        ]);
     }
 }
