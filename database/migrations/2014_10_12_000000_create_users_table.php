33767fab937920cc9d9614d4d5afb4d2df4cf8cf
CablePoolV4 <vegitoson01@gmail.com>
2021-02-03 22:55:17 +0700

    add coin, game_total to user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->bigInteger('coin');
+            $table->bigInteger('total_game');
             $table->timestamps();
         });
     }
